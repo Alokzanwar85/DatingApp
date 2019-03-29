44439a695fc6a890729379dac5f69552f500e43d
Alokzanwar85 <alokzanwar85@gmail.com>
2019-03-29 11:29:46 +0530

    Initial Commit  - ProfileApp UI and API

---
+++ ProfileApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProfileApp.API.Data;
+
+namespace ProfileApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        
+        public DataContext DataContext { get; set;}
+
+        public ValuesController(DataContext dataContext)
+        {
+            DataContext = dataContext;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValuesAsync()
+        {
+            var values=await DataContext.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValueAsync(int id)
+        {
+            try{
+
+             var value=await DataContext.Values.FirstOrDefaultAsync(i=>i.Id==id);
+
+             return Ok(value);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest();
+
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ProfileApp.API/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+//using Microsoft.AspNetCore.SqlLite;
+
+using ProfileApp.API.Data;
+
+
+namespace ProfileApp.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method toet add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<DataContext>(x=>x.UseSqlite(this.Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseCors(x=>x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseMvc();
+        }
+    }
+}
